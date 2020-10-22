4300ca2ef8b6152660142cc8c613f0223516f6b4
Josephpark0720 <70733078+Josephpark0720@users.noreply.github.com>
2020-10-22 21:30:30 +0900

    final 2

--- app.py
+++ app.py
@@ -43,6 +43,10 @@ def form():
 def post():
     return render_template('post.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0')
